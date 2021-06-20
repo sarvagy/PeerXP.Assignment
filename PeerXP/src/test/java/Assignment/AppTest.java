b0c1e812b7deb99e544e8988bb44fbcbe0bde64d
sarvagy <sarvagygulati1@gmail.com>
2021-06-20 11:44:16 +0530

    First Commit

---
+++ PeerXP/src/test/java/Assignment/AppTest.java
@@ -0,0 +1,20 @@
+package Assignment;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertEquals( true,true );
+    }
+}
